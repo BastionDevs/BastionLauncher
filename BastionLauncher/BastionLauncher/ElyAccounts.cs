a70ee563b5c03d7ff18bc22238469978cbfbf9c3
Andrew <itsandrewplayz@hotmail.com>
2024-09-23 11:35:58 +0800

    Added Ely auth handling

---
+++ BastionLauncher/BastionLauncher/ElyAccounts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BastionLauncher
+{
+    class ElyAccounts
+    {
+
+        public static string PwdAuth(string username, string password, string clienttoken, bool requestuser)
+        {
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/authenticate", "{\r\n  \"username\": \""+username+"\",\r\n  \"password\": \""+password+"\",\r\n  \"clientToken\": \""+clienttoken+"\",\r\n  \"requestUser\": \""+requestuser+"\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return resp; } else { return "ERROR:"+resp; }
+        }
+
+        public static string Refresh(string accesstoken)
+        {
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/refresh", "{\r\n  \r\n  \"accessToken\": \"" + accesstoken + "\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return resp; } else { return "ERROR:" + resp; }
+        }
+
+        public static bool ValidateToken(string username, string password, string clienttoken, bool requestuser)
+        {
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/validate", "{\r\n  \"username\": \"" + username + "\",\r\n  \"password\": \"" + password + "\",\r\n  \"clientToken\": \"" + clienttoken + "\",\r\n  \"requestUser\": \"" + requestuser + "\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return true; } else { return false; }
+        }
+
+        public static bool Signout(string username, string password)
+        {
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/signout", "{\r\n  \"username\": \"" + username + "\",\r\n  \"password\": \"" + password + "\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return true; } else { return false; }
+        }
+
+        public static bool InvalidateToken(string accessToken, string clientToken)
+        {
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/invalidate", "{\r\n  \"accessToken\": \"" + accessToken + "\",\r\n  \"clientToken\": \"" + clientToken + "\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return true; } else { return false; }
+        }
+
+    }
+}
---
+++ BastionLauncher/BastionLauncher/NetUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BastionLauncher
+{
+    internal class NetUtils
+    {
+        //https://stackoverflow.com/a/10027534
+        public static string POSTReqJSON(string url, string json)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+    }
+}
