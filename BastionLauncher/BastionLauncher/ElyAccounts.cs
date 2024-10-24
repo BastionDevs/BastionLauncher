19f4669c99e7fc180e8b83d8b5d514843cb8e883
Andrew <itsandrewplayz@hotmail.com>
2024-10-24 23:03:11 +0800

    Revert "Access Token validation for Ely.by accounts"
    This reverts commit 64e5a0b7ed895995e6204b37d9ee1b482ba3df27.

--- BastionLauncher/BastionLauncher/ElyAccounts.cs
+++ BastionLauncher/BastionLauncher/ElyAccounts.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,49 +21,10 @@ namespace BastionLauncher
             if (!resp.Contains("{\r\n    \"error\": \"")) { return resp; } else { return "ERROR:" + resp; }
         }
 
-        public static bool ValidateToken(string accesstoken)
+        public static bool ValidateToken(string username, string password, string clienttoken, bool requestuser)
         {
-            try
-            {
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://authserver.ely.by/auth/validate");
-                httpWebRequest.ContentType = "application/json";
-                httpWebRequest.Method = "POST";
-
-                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-                {
-                    streamWriter.Write("{\r\n \"accessToken\": \"" + accesstoken + "\"\r\n}");
-                }
-
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
-                    if (string.IsNullOrEmpty(streamReader.ReadToEnd()))
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-            }
-            catch (WebException ex)
-            {
-                if (ex.Response != null)
-                {
-                    using (var errorResponse = (HttpWebResponse)ex.Response)
-                    {
-                        using (var reader = new StreamReader(errorResponse.GetResponseStream()))
-                        {
-                            string error = reader.ReadToEnd();
-                            // Return or log error details along with status code
-                            Console.Write($"Error: {errorResponse.StatusCode}, Body: {error}");
-                            return false;
-                        }
-                    }
-                } else { return false; }
-
-            }
+            string resp = NetUtils.POSTReqJSON($"https://authserver.ely.by/auth/validate", "{\r\n  \"username\": \"" + username + "\",\r\n  \"password\": \"" + password + "\",\r\n  \"clientToken\": \"" + clienttoken + "\",\r\n  \"requestUser\": \"" + requestuser + "\"\r\n}");
+            if (!resp.Contains("{\r\n    \"error\": \"")) { return true; } else { return false; }
         }
 
         public static bool Signout(string username, string password)
