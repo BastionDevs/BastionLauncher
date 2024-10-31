5822aea674da3f512ce4c831f837a2adcffc5d2c
Andrew <itsandrewplayz@hotmail.com>
2024-10-31 14:33:04 +0800

    Parse for Mojang/MS users in Form1

--- BastionLauncher/BastionLauncher/Form1.cs
+++ BastionLauncher/BastionLauncher/Form1.cs
@@ -29,10 +29,15 @@ namespace BastionLauncher
             pictureBox1.Size = new Size(100, 100);
             // Check if the "elyusers" token is null
             JToken elyusersToken = Util.LauncherUserProfiles["elyusers"];
+            JToken mojangusersToken = Util.LauncherUserProfiles["mojangusers"];
+
+            bool elyuserspresent;
+            bool mojanguserspresent;
+
+            comboBox2.Items.Clear();
 
             if (elyusersToken != null && elyusersToken.HasValues) // Check if "elyusers" exists and has players
             {
-                comboBox2.Items.Clear(); // Clear existing items first
                 comboBox2.Items.Add("= Ely.by Accounts =");
 
                 foreach (JProperty player in elyusersToken) // Loop through each player
@@ -45,8 +50,33 @@ namespace BastionLauncher
                 {
                     comboBox2.SelectedIndex = 1; // Select the first item
                 }
+                elyuserspresent = true;
+            } else
+            {
+                elyuserspresent = false;
+            }
+
+            if (mojangusersToken != null && mojangusersToken.HasValues) // Check if "mojangusers" exists and has players
+            {
+                comboBox2.Items.Add("= Mojang/MS Accounts =");
+
+                foreach (JProperty player in mojangusersToken) // Loop through each player
+                {
+                    comboBox2.Items.Add(player.Name); // Add player names to ComboBox
+                }
+
+                // Automatically select the first player if any exist
+                if (comboBox2.Items.Count > 0)
+                {
+                    comboBox2.SelectedIndex = 1; // Select the first item
+                }
+                mojanguserspresent = true;
             }
             else
+            {
+                mojanguserspresent = false;
+            }
+            if (!elyuserspresent || !mojanguserspresent)
             {
                 // If no users exist, add the placeholder item "<Add an account to continue>"
                 comboBox2.Items.Clear();
