3e5442f822ac386c4e9e5e1f3df9ad9d656e333a
Andrew <itsandrewplayz@hotmail.com>
2024-11-01 15:36:43 +0800

    MSA parsing in Account Manager

--- BastionLauncher/BastionLauncher/AccountManager.cs
+++ BastionLauncher/BastionLauncher/AccountManager.cs
@@ -21,11 +21,17 @@ namespace BastionLauncher
 
         private void AccountManager_Load(object sender, EventArgs e)
         {
+            // Check if the "elyusers" token is null
             JToken elyusersToken = Util.LauncherUserProfiles["elyusers"];
+            JToken mojangusersToken = Util.LauncherUserProfiles["mojangusers"];
+
+            bool elyuserspresent;
+            bool mojanguserspresent;
+
+            listBox1.Items.Clear();
 
             if (elyusersToken != null && elyusersToken.HasValues) // Check if "elyusers" exists and has players
             {
-                listBox1.Items.Clear(); // Clear existing items first
                 listBox1.Items.Add("= Ely.by Accounts =");
 
                 foreach (JProperty player in elyusersToken) // Loop through each player
@@ -36,10 +42,36 @@ namespace BastionLauncher
                 // Automatically select the first player if any exist
                 if (listBox1.Items.Count > 0)
                 {
-                    listBox1.SelectedIndex = 0; // Select the first item
+                    listBox1.SelectedIndex = 1; // Select the first item
                 }
+                elyuserspresent = true;
             }
             else
+            {
+                elyuserspresent = false;
+            }
+
+            if (mojangusersToken != null && mojangusersToken.HasValues) // Check if "mojangusers" exists and has players
+            {
+                listBox1.Items.Add("= Mojang/MS Accounts =");
+
+                foreach (JProperty player in mojangusersToken) // Loop through each player
+                {
+                    listBox1.Items.Add(player.Name); // Add player names to ComboBox
+                }
+
+                // Automatically select the first player if any exist
+                if (listBox1.Items.Count > 0)
+                {
+                    listBox1.SelectedIndex = 1; // Select the first item
+                }
+                mojanguserspresent = true;
+            }
+            else
+            {
+                mojanguserspresent = false;
+            }
+            if (!elyuserspresent || !mojanguserspresent)
             {
                 // If no users exist, add the placeholder item "<Add an account to continue>"
                 listBox1.Items.Clear();
