80c60bc00d0cbf1f2f73a167f934dadb3cafcf85
Andrew <itsandrewplayz@hotmail.com>
2024-11-01 17:18:50 +0800

    Basic account adding screen

---
+++ BastionLauncher/BastionLauncher/ManageAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BastionLauncher
+{
+    public partial class ManageAccount : Form
+    {
+        public ManageAccount()
+        {
+            InitializeComponent();
+        }
+
+        public ManageAccount(string username)
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton1.Checked = false;
+            }
+        }
+    }
+}
